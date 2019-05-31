7842eb993255e101839726c05c93a9be9fbcbaeb
Tamanegi <3nin10iro@gmail.com>
2019-05-31 11:26:11 +0900

    とりあえずsassとslimが動く状態
    includeがまだ動きまへん

--- gulpfile.js
+++ gulpfile.js
@@ -5,47 +5,45 @@ const autoprefixer = require("autoprefixer");
 const slim         = require("gulp-slim");
 const plumber      = require("gulp-plumber");
 
-gulp.task('slim', function(){
-  gulp.src('*.slim')
+// sass
+gulp.task("style", function() {
+  return (
+    gulp
+      .src("css/*.sass")
+      // Sassのコンパイルを実行
+      .pipe(
+        sass({
+          outputStyle: "compressed"
+        })
+      )
+      .pipe(postcss([
+        autoprefixer({
+          // IEは11以上、Androidは4.4以上
+          // その他は最新2バージョンで必要なベンダープレフィックスを付与する設定
+          browsers: ["last 2 versions", "ie >= 11", "Android >= 4"],
+          cascade: false,
+          grid: true
+        })
+      ]))
+      // cssフォルダー以下に保存
+      .pipe(gulp.dest("css"))
+  );
+});
+
+// slim
+gulp.task('slim', function() {
+  return gulp.src('slim/*.slim')
     .pipe(plumber())
     .pipe(slim({
-      pretty:  true,
-      require: 'slim/include',
-      options: 'include_dirs=["slim/includes"]'
+      pretty: true
     }))
-    .pipe(gulp.dest(""));
+    .pipe(gulp.dest('./'))
 });
 
-
-gulp.task('default', function() {
-  gulp.watch(['*.slim', gulp.series(['slim']));
+// watch
+gulp.task('watch', () => {
+  gulp.watch('css/*.sass', gulp.task('style'));
+  gulp.watch('slim/*.slim', gulp.task('slim'));
 });
 
-// gulp.task("style", function() {
-//   return gulp.watch(["css/*.sass","slim/*.slim"], function() {
-//     return (
-//       gulp
-//         .src("css/*.sass")
-//         // Sassのコンパイルを実行
-//         .pipe(
-//           sass({
-//             outputStyle: "compressed"
-//           })
-//             // Sassのコンパイルエラーを表示
-//             // (これがないと自動的に止まってしまう)
-//             .on("error", sass.logError)
-//         )
-//         .pipe(postcss([
-//           autoprefixer({
-//             // IEは11以上、Androidは4.4以上
-//             // その他は最新2バージョンで必要なベンダープレフィックスを付与する設定
-//             browsers: ["last 2 versions", "ie >= 11", "Android >= 4"],
-//             cascade: false,
-//             grid: true
-//           })
-//         ]))
-//         // cssフォルダー以下に保存
-//         .pipe(gulp.dest("css"))
-//     );
-//   });
-// });
+gulp.task('default', gulp.parallel('watch'));
