6fc9dd7051c0c8cb8e93d52f84c6cccf00692c19
Tamanegi <3nin10iro@gmail.com>
2019-06-14 10:33:01 +0900

    glupで整理
    出力ソースはdistへ
    作業ディレクトリはsrcへ

--- gulpfile.js
+++ gulpfile.js
@@ -9,8 +9,7 @@ const plumber      = require("gulp-plumber");
 gulp.task("style", function() {
   return (
     gulp
-      .src("css/*.sass")
-      // Sassのコンパイルを実行
+      .src("src/sass/*.sass")
       .pipe(
         sass({
           outputStyle: "expanded"
@@ -18,34 +17,32 @@ gulp.task("style", function() {
       )
       .pipe(postcss([
         autoprefixer({
-          // IEは11以上、Androidは4.4以上
-          // その他は最新2バージョンで必要なベンダープレフィックスを付与する設定
+          // 最新2バージョンで必要なベンダープレフィックスを付与する設定
           browsers: ["last 2 versions", "ie >= 11", "Android >= 4"],
           cascade: false,
           grid: true
         })
       ]))
-      // cssフォルダー以下に保存
-      .pipe(gulp.dest("css"))
+      .pipe(gulp.dest("dist/css"))
   );
 });
 
 // slim
 gulp.task('slim', function() {
-  return gulp.src('slim/*.slim')
+  return gulp.src('src/slim/*.slim')
     .pipe(plumber())
     .pipe(slim({
       pretty: true,
       require: 'slim/include',
-      options: 'include_dirs=["slim/inc"]'
+      options: 'include_dirs=["src/slim/inc"]'
     }))
     .pipe(gulp.dest('./'))
 });
 
 // watch
 gulp.task('watch', () => {
-  gulp.watch('css/*.sass', gulp.task('style'));
-  gulp.watch(['slim/*.slim', 'slim/inc/*.slim'], gulp.task('slim'));
+  gulp.watch('src/sass/*.sass', gulp.task('style'));
+  gulp.watch(['src/slim/*.slim', 'src/slim/inc/*.slim'], gulp.task('slim'));
 });
 
 gulp.task('default', gulp.parallel('watch'));
